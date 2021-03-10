c85837c099bde4c287ab30210edac079617aa814
aman-007-ia <59302337+aman-007-ia@users.noreply.github.com>
2021-03-10 09:05:56 +0530

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int binarysearch(int a[], int n , int key){
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        int mid= (s+e)/2;
+        if(a[mid]==key){
+            return mid;
+        }
+        else if (a[mid]>key){
+            e = mid-1;
+        }
+        else{
+            s = mid+1;
+        }
+    }
+    return-1;
+}
+int main() {
+	int n,key;
+	cin >>n;
+	int a[1000];
+	for(int i=0 ;i<n;i++){
+	    cin>>a[i];
+	}
+	cout<<"Enter the element you want to search : ";
+	cin>>key;
+	cout<<binarysearch(a,n,key)<<endl;
+}
