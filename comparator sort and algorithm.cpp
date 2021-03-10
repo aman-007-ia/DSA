260a930dd08a55694d7bbe1dc6ff846c5b73b0ba
aman-007-ia <59302337+aman-007-ia@users.noreply.github.com>
2021-03-10 12:28:12 +0530

    Add files via upload

---
+++ comparator sort and algorithm.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+bool compare(int a,int y){
+    cout<<"compare "<<a<<" and "<<"compare "<<y<<endl;
+    return a<y;
+}
+void bubble_sort(int a[],int n,bool (&cmp)(int a ,int y)){
+    for(int i=1;i<=n-1;i++){
+        for(int j=0;j<=n-i-1;j++){
+            if(a[j],a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
+int main() {
+	int n,key;
+	cin >>n;
+	int a[1000];
+	for(int i=0 ;i<n;i++){
+	    cin>>a[i];
+	}
+	sort(a,a+n,compare);
+	for(int i=0;i<n;i++){
+	    cout<<a[i]<<"  ";
+	}
+}
