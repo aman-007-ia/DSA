6cdc0b009396818b71748c496fbdd4408c8d3041
aman-007-ia <59302337+aman-007-ia@users.noreply.github.com>
2021-03-10 11:21:58 +0530

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void bubble_sort(int a[],int n){
+    for(int i=1;i<=n-1;i++){
+        for(int j=0;j<=n-i-1;j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
+int main() {
+	int n,key;
+	cin >>n;
+	int a[1000];
+	for(int i=0 ;i<n;i++){
+	    cin>>a[i];
+	}
+	bubble_sort(a,n);
+	for(int i=0;i<n;i++){
+	    cout<<a[i]<<"  ";
+	}
+}
